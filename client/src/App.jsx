f863bcb2920b9d588ceb8c3fbe61e8425e6630e1
TusharJoy <tusharghoshjoy@gmail.com>
2025-04-19 02:18:55 +0600

    added dark mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import Layout from '@/components/Layout'
 import Home from '@/pages/Home'
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
-      <Toaster />
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+        <Toaster />
+      </Router>
+    </ThemeProvider>
   )
 }
 
